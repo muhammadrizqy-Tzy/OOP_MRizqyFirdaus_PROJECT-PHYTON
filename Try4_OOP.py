16c2844a3bf08f767fb01e127c56240b7014e60a
Muhammad Rizqy Firdaus <sn528522@gmail.com>
2026-04-09 07:56:04 +0700

    Add files via upload

---
+++ Try4_OOP.py
@@ -0,0 +1,16 @@
+a=10
+b=3
+
+print("X=a+b")
+print("X=",a+b)
+print("Y=a-b")
+print("Y=",a-b)
+print("C=a*b")
+print("C=",a*b)
+print("Z=a/b")
+print("Z=",a/b)
+print("P=a**b")
+print("P=",a**b)
+print("W=a%b")
+print("W=",a%b)
+
