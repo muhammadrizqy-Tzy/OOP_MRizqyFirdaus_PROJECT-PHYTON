e158b204cb18212cb97ddbc5b4cf9d9168757537
Muhammad Rizqy Firdaus <sn528522@gmail.com>
2026-04-21 11:47:31 +0700

    Add files via upload

--- try9_OOP.py
+++ try9_OOP.py
@@ -1,1 +1,9 @@
+a=5
+b=8
 
+print("a & b =", a & b, bin(a & b))
+print("a | b =", a | b, bin(a | b))
+print("a ^ b =", a ^ b, bin(a ^ b))
+print("~a =", ~a, bin(~a))
+print("a << 1 =", a << 1, bin(a << 1))
+print("a >> 1 =", a >> 1, bin(a >> 1))
